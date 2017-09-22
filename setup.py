3dc5a051cefd4c70fb4acd442a3f917c2e8c4614
Zhang, Yuxuan <zhangy6@ornl.gov>
2017-09-22 15:38:35 -0400

    fixed to only include /data/_mock_data folder.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author="Yuxuan Zhang, Jean C. Bilheux",
     author_email="zhangy6@ornl.gov, bilheuxjm@ornl.gov",
     packages=find_packages(exclude=['tests', 'notebooks']),
-    package_data={'ResoFit': ['data/*']},
+    package_data={'ResoFit': ['data/_mock_data_for_test/*']},
     include_package_data=True,
     test_suite='tests',
 
--- tests/ResoFit/test_calibration.py
+++ tests/ResoFit/test_calibration.py
@@ -1,5 +1,5 @@
 import unittest
-from ResoFit.simulation import Simulation
+from ResoFit.calibration import Calibration
 import numpy as np
 
 
@@ -7,3 +7,6 @@ class TestCalibration(unittest.TestCase):
     energy_min = 7
     energy_max = 10
     energy_step = 1
+
+    # def test_vary_tag(self):
+    #     calibration = Calibration
