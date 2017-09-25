a37a815980dc2042e326e67f2ab671ebf1faff8a
Zhang, Yuxuan <zhangy6@ornl.gov>
2017-09-25 17:30:36 -0400

    fixing docs

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -34,10 +34,13 @@ import sphinx_rtd_theme
 # ones.
 extensions = ['sphinx.ext.viewcode',
               'nbsphinx',
-              'sphinx.ext.mathjax']
+              'sphinx.ext.mathjax',
+              'IPython.sphinxext.ipython_console_highlighting']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
