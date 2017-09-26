f87882aa57859934f84240a936aafd4e7aa0688e
Zhang, Yuxuan <zhangy6@ornl.gov>
2017-09-26 09:21:00 -0400

    readthedocs settings

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -41,8 +41,6 @@ extensions = ['sphinx.ext.viewcode',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
@@ -76,7 +74,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = []
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
