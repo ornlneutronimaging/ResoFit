49bb0d3857dd0c046246c0ea2287ed3e5fcf57d5
Zhang, Yuxuan <zhangy6@ornl.gov>
2017-10-11 09:18:38 -0400

    added test data

--- ResoFit/IPTS_13639_W.py
+++ ResoFit/IPTS_13639_W.py
@@ -7,20 +7,15 @@ import pprint
 from ResoFit._utilities import get_foil_density_gcm3
 from ResoFit._utilities import Layer
 
-
 # Global parameters
-energy_min = 7
-energy_max = 150
+energy_min = 14
+energy_max = 300
 energy_step = 0.01
 # Input sample name or names as str, case sensitive
-# layer = 'UGd'
-# thickness = 0.018  # mm
-# density = get_foil_density_gcm3(length_mm=25, width_mm=25, thickness_mm=0.025, mass_g=0.14)
-# density = None
-# density = 8.86
 layer_1 = 'W'
-thickness_1 = 0.018
+thickness_1 = 0.05  # mm
 density_1 = None
+# density = get_foil_density_gcm3(length_mm=25, width_mm=25, thickness_mm=0.025, mass_g=0.14)
 # layer_2 = 'Gd'
 # thickness_2 = 0.015
 # density_2 = None
@@ -29,16 +24,14 @@ density_1 = None
 # density_3 = None
 layer = Layer()
 layer.add_layer(layer=layer_1, thickness_mm=thickness_1, density_gcm3=density_1)
-# layer.add_layer(layer=layer_2, thickness_mm=thickness_2, density_gcm3=density_2)
-# layer.add_layer(layer=layer_3, thickness_mm=thickness_3, density_gcm3=density_3)
 
-folder = 'data'
+folder = 'data/resonance_data/IPTS_13639'
 data_file = 'W.csv'
-spectra_file = 'Image002_Spectra.txt'
-image_start = None  # Can be omitted or =None
-image_end = None  # Can be omitted or =None
-norm_to_file = None#'run_33_resonance_ob.txt'#None  # 'sphere_background_1.csv'
-baseline = False
+spectra_file = 'spectra.csv'
+image_start = 500  # Can be omitted or =None
+image_end = 1600  # Can be omitted or =None
+# norm_to_file = 'Ag.csv'
+baseline = True
 each_step = False
 
 repeat = 1
@@ -56,18 +49,19 @@ calibration = Calibration(data_file=data_file,
                           folder=folder,
                           baseline=baseline)
 
-calibration.norm_to(norm_to_file)
+# calibration.norm_to(norm_to_file)
 calibration.slice(slice_start=image_start, slice_end=image_end)
 
 calibrate_result = calibration.calibrate(source_to_detector_m=source_to_detector_m,
                                          offset_us=offset_us,
                                          vary='all',
                                          each_step=each_step)
-# calibration.plot_before()
-# calibration.plot_after()
+calibration.plot_before()
+calibration.plot_after()
 
 # Fit the peak height
-fit = FitResonance(spectra_file=spectra_file,
+fit = FitResonance(folder=folder,
+                   spectra_file=spectra_file,
                    data_file=data_file,
                    repeat=repeat,
                    energy_min=energy_min,
@@ -75,11 +69,11 @@ fit = FitResonance(spectra_file=spectra_file,
                    energy_step=energy_step,
                    calibrated_offset_us=calibration.calibrated_offset_us,
                    calibrated_source_to_detector_m=calibration.calibrated_source_to_detector_m,
-                   norm_to_file=norm_to_file,
+                   # norm_to_file=norm_to_file,
                    slice_start=image_start,
                    slice_end=image_end,
                    baseline=baseline)
-fit.fit(layer, vary='density', each_step=each_step)
+fit.fit(layer, vary='thickness', each_step=each_step)
 fit.molar_conc()
 fit.plot_before()
-fit.plot_after(error=True)
+fit.plot_after()
